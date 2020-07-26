8cea1070be3366c6c3352600fd1405236a4afc84
Anuragtech02 <apal895@gmail.com>
2020-07-26 13:01:25 +0530

    Complete Responsive

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
     $number = $_POST['number'];
     $branch = $_POST['branch'];
 
-    $to = "apal895@gmail.com";
+    $to = "";
     $subject = "IIT Pulse Form";
     $message = "Name: $name.\n".
                     "Class : $std.\n".
@@ -13,6 +13,12 @@
                             "Branch : $branch.\n";
                             
     $headers = 'From: contact@iitpulse.com' . "\r\n";
+
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+    $headers .= "X-Priority: 3\r\n";
+    $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
+
 
     if(mail($to, $subject, $message, $headers)){
         echo "Successfully Submitted";
